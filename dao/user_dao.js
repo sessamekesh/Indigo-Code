4bac9ec95c9ef7ef6318cef0732765ee3823076b
sessamekesh <kamaron.peterson@aggiemail.usu.edu>
2015-05-07 19:38:00 -0600

    (v0.3.1) I know I shouldn't synchronize work between computers using GitHub, but I do anyway.

--- dao/user_dao.js
+++ dao/user_dao.js
@@ -42,24 +42,31 @@ exports.create_new_user = function (username, password, email, is_admin, cb) {
                                     // Generate salt for encryption...
                                     bcrypt.genSalt(work_factor, function (cerr, salt) {
                                         if (cerr) {
-                                            cb(cerr);
-                                        } else {
-                                            
-                                        }
-                                    });
-                                    collection.insertOne({
-                                        user_id: uid,
-                                        username: username,
-                                        // TODO KIP: Pass hash here
-                                        email: email,
-                                        is_admin: is_admin
-                                    }, function (cerr, cres) {
-                                        if (cerr) {
                                             db.close();
                                             cb(cerr);
                                         } else {
-                                            db.close();
-                                            cb(uid);
+                                            bcrypt.hash(password, salt, function (derr, pass_hash) {
+                                                if (derr) {
+                                                    db.close();
+                                                    cb(derr);
+                                                } else {
+                                                    collection.insertOne({
+                                                        user_id: uid,
+                                                        username: username,
+                                                        pass_hash: pass_hash,
+                                                        email: email,
+                                                        is_admin: is_admin
+                                                    }, function (cerr, cres) {
+                                                        if (cerr) {
+                                                            db.close();
+                                                            cb(cerr);
+                                                        } else {
+                                                            db.close();
+                                                            cb(null, uid);
+                                                        }
+                                                    });
+                                                }
+                                            });
                                         }
                                     });
                                 }
@@ -92,7 +99,7 @@ exports.get_user_data = function (user_id, cb) {
                             cb();
                         } else {
                             db.close();
-                            cb(aresults[0]);
+                            cb(null, aresults[0]);
                         }
                     }
                 });
@@ -102,23 +109,67 @@ exports.get_user_data = function (user_id, cb) {
 };
 
 exports.get_user_by_username = function (username, cb) {
-    if (username === 'sessamekesh') {
-        cb(null, {
-            user_id: 0,
-            username: 'sessamekesh',
-            pass_hash: 'asdf',
-            email_address: 'kam.is.amazing@gmail.com',
-            is_admin: true
-        });
+    if (username === undefined || username === '') {
+        cb('No valid username provided');
     } else {
-        cb('User ' + username + ' does not exist!', undefined);
+        MongoClient.connect(connection_settings.url, function (err, db){
+            if (err) {
+                db.close();
+                cb(err);
+            } else {
+                var collection = db.collection('user_data');
+                collection.find({ username: username }).toArray(function (aerr, ares) {
+                    if (aerr) {
+                        db.close();
+                        cb(aerr);
+                    } else {
+                        if (ares.length === 0) {
+                            db.close();
+                            cb();
+                        } else {
+                            db.close();
+                            cb(null, ares[0]);
+                        }
+                    }
+                });
+            }
+        });
     }
 };
 
 exports.authenticate_user = function (username, password, cb) {
-    if (username === 'sessamekesh' && password === 'asdf') {
-        cb(null, true);
+    if (username === undefined || username === '') {
+        cb('Must provide valid username');
+    } else if (password === undefined || password === '') {
+        cb('Must provide password');
     } else {
-        cb(null, false);
+        MongoClient.connect(connection_settings.url, function (err, db) {
+            if (err) {
+                db.close();
+                cb(err);
+            } else {
+                var collection = db.collection('user_data');
+                collection.find({ username: username }).toArray(function (aerr, ares) {
+                    if (aerr) {
+                        db.close();
+                        cb(aerr);
+                    } else {
+                        if (ares.length === 0){
+                            db.close();
+                            cb(undefined, false);
+                        } else {
+                            bcrypt.compare(password, ares[0].pass_hash, function (berr, bres) {
+                                if (berr) {
+                                    db.close();
+                                    cb(berr);
+                                } else {
+                                    cb(null, bres);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 };
