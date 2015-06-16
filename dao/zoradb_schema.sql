060b0072d9b86f1fe6cd71a72b6d23842a16431c
sessamekesh <kamaron.peterson@aggiemail.usu.edu>
2015-06-15 20:56:59 -0600

    (v0.3.1) Updated zoradb_schema to include Use zoradb statement

--- dao/zoradb_schema.sql
+++ dao/zoradb_schema.sql
@@ -36,7 +36,7 @@
 --
 
 CREATE DATABASE zoradb;
-
+USE zoradb;
 DROP TABLE IF EXISTS `allowed_language`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
